78bddb26b45d7d02e511e123b5064b830e0e4ffe
larryhou <larryhou@foxmail.com>
2014-08-26 23:14:19 +0800

    Swift版本Regions

---
+++ Regions/Regions/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Regions
+//
+//  Created by doudou on 8/26/14.
+//  Copyright (c) 2014 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+                            
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
