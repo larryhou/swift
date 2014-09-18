4973fa7d193ba30c1254d3f1f8b650a268c0ef9c
larryhou <larryhou@foxmail.com>
2014-09-18 15:06:00 +0800

    Teslameter project initialization

---
+++ Teslameter/Teslameter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Teslameter
+//
+//  Created by larryhou on 9/18/14.
+//  Copyright (c) 2014 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
