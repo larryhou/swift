2895da44397977c657411dd606d077af278750d3
larryhou <larryhou@foxmail.com>
2015-03-24 20:07:53 +0800

    NSURLSession usage

---
+++ URLoadingSystem/URLoadingSystem/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  URLoadingSystem
+//
+//  Created by larryhou on 3/23/15.
+//  Copyright (c) 2015 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		
+		performSessionTask()
+	}
+	
+	func performSessionTask()
+	{
+		var proxy:[NSObject:AnyObject] = [:]
+		proxy.updateValue(kCFProxyTypeHTTP, forKey: kCFProxyTypeKey)
+		proxy.updateValue("proxy.tencent.com", forKey: kCFProxyHostNameKey)
+		proxy.updateValue("8080", forKey: kCFProxyPortNumberKey)
+		
+		let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+		session.downloadTaskWithURL(NSURL(string: "https://www.baidu.com/")!, completionHandler:
+		{
+			(url:NSURL!, response:NSURLResponse!, error:NSError!) in
+			
+			let cfencoding = CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue)
+			let nsencoding = CFStringConvertEncodingToNSStringEncoding(cfencoding)
+			
+			let contents = NSString(data: NSData(contentsOfURL: url)!, encoding: nsencoding)
+			println(contents)
+			println(response)
+			
+		}).resume()
+	}
+
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
+
