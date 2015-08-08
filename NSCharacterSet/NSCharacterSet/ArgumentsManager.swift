537066568d993737db6207afb9b499787804b8fc
larryhou <larryhou@foxmail.com>
2015-08-09 00:41:22 +0800

    NSCharacterSet develop tool

---
+++ NSCharacterSet/NSCharacterSet/ArgumentsManager.swift
@@ -0,0 +1,129 @@
+//
+//  ArgumentsManager.swift
+//  urlcodec
+//
+//  Created by larryhou on 1/8/2015.
+//  Copyright © 2015 larryhou. All rights reserved.
+//
+
+import Foundation
+
+class ArgumentsManager
+{
+    class ArgumentOption
+    {
+        let name:String, abbr:String, help:String, hasValue:Bool, trigger:()->Void
+        var value:String?
+        
+        init(name:String, abbr:String, help:String, hasValue:Bool, trigger:()->Void)
+        {
+            self.name = name
+            self.abbr = abbr
+            self.help = help
+            self.hasValue = hasValue
+            self.trigger = trigger
+        }
+    }
+    
+    private var map:[String:ArgumentOption]
+    private var options:[ArgumentOption]
+    
+    private var pattern:NSRegularExpression!
+    
+    init()
+    {
+        self.options = []
+        self.map = [:]
+        
+        do
+        {
+            pattern = try NSRegularExpression(pattern: "^-[a-z]+|--[a-z_-]{2,}$", options: NSRegularExpressionOptions.CaseInsensitive)
+        }
+        catch {}
+    }
+    
+    func insertOption(name:String, abbr:String, help:String, hasValue:Bool, trigger:()->Void)
+    {
+        if name == "" && abbr == ""
+        {
+            return
+        }
+        
+        let argOption = ArgumentOption(name: name, abbr: abbr, help: help, hasValue: hasValue, trigger: trigger)
+        options.append(argOption)
+        
+        map[name] = argOption
+        map[abbr] = argOption
+    }
+    
+    func getOption(name:String) -> ArgumentOption?
+    {
+        return map[name]
+    }
+    
+    func recognizeOption(value:String, triggerWhenMatch:Bool = false) -> Bool
+    {
+        let matches = pattern.matchesInString(value,
+            options: NSMatchingOptions.ReportProgress,
+            range: NSRange(location: 0, length: NSString(string: value).length))
+        if matches.count > 0
+        {
+            if triggerWhenMatch
+            {
+                trigger(value)
+            }
+            
+            return true
+        }
+        
+        return false
+    }
+    
+    func trigger(name:String)
+    {
+        map[name]?.trigger()
+    }
+    
+    func padding(var value:String, length:Int, var filling:String = " ") -> String
+    {
+        if NSString(string: filling).length == 0
+        {
+            filling = " "
+        }
+        else
+        {
+            filling = filling.substringToIndex(filling.startIndex.successor())
+        }
+        
+        while NSString(string: value).length < length
+        {
+            value += filling
+        }
+        return value
+    }
+    
+    func showHelpMessage(stream:UnsafeMutablePointer<FILE> = stdout)
+    {
+        var maxNameLength = 0
+        var maxAbbrLength = 0
+        
+        var abbrs:[String] = []
+        for var i = 0; i < options.count; i++
+        {
+            maxNameLength = max(maxNameLength, NSString(string: options[i].name).length)
+            maxAbbrLength = max(maxAbbrLength, NSString(string: options[i].abbr).length)
+            abbrs.append(options[i].abbr)
+        }
+        
+        fputs("urlcodec " + " ".join(abbrs) + " String ...\n", stream)
+        
+        for i in 0 ..< options.count
+        {
+            let item = options[i]
+            var help = padding(item.abbr, length: maxAbbrLength)
+            help += item.name == "" || item.abbr == "" ? " " : ","
+            help += " " + padding(item.name, length: maxNameLength) + "\t" + item.help
+            fputs(help + "\n", stream)
+        }
+    }
+}
---
+++ NSCharacterSet/NSCharacterSet/main.swift
@@ -0,0 +1,126 @@
+//
+//  main.swift
+//  NSCharacterSet
+//
+//  Created by larryhou on 8/8/2015.
+//  Copyright © 2015 larryhou. All rights reserved.
+//
+
+import Foundation
+
+var arguments = Process.arguments
+arguments = Array(arguments[1..<arguments.count])
+
+var name = "URLHostAllowedCharacterSet"
+var renderUnicodeAsText = false
+var column = 16
+var inverse = false
+var verbose = false
+
+let manager = ArgumentsManager()
+manager.insertOption("--character-set", abbr: "-s", help: "Property name of NSCharacterSet class", hasValue: true) { name = manager.getOption("-s")!.value! }
+manager.insertOption("--render-unicode", abbr: "-u", help: "Render unicode as text", hasValue: false) { renderUnicodeAsText = true }
+manager.insertOption("--inverse", abbr: "-i", help: "Inverse current character set", hasValue: false) { inverse = true }
+manager.insertOption("--verbose", abbr: "-v", help: "Enable verbose printing", hasValue: false) { verbose = true }
+manager.insertOption("--hex-columns", abbr: "-c", help: "Set hex columns in verbose printing", hasValue: true)
+{
+    let value = NSString(string: manager.getOption("-c")!.value!)
+    column = max(1, value.integerValue)
+}
+manager.insertOption("--help", abbr: "-h", help: "Show help message", hasValue: false)
+{
+    manager.showHelpMessage()
+    exit(0)
+}
+
+while arguments.count > 0
+{
+    let text = arguments[0]
+    if manager.recognizeOption(text)
+    {
+        arguments.removeAtIndex(0)
+        if var option = manager.getOption(text) where option.hasValue
+        {
+            let value = arguments[0]
+            if manager.recognizeOption(value) == false
+            {
+                arguments.removeAtIndex(0)
+                option.value = value
+            }
+            else
+            {
+                fputs("\(text): missing argument value\n", stderr)
+                exit(1)
+            }
+        }
+        
+        manager.getOption(text)?.trigger()
+    }
+}
+if verbose
+{
+    print("Arguments: \(Process.arguments)")
+    print("NSCharacterSet.\(name)")
+}
+
+let valueObject = NSCharacterSet.valueForKeyPath(name)
+if !(valueObject is NSCharacterSet)
+{
+    fputs("NSCharacterSet.\(name): not exist!", stderr)
+    exit(2)
+}
+
+let bitmap:NSData
+if inverse == false
+{
+    bitmap = (valueObject as! NSCharacterSet).bitmapRepresentation
+}
+else
+{
+    bitmap = (valueObject as! NSCharacterSet).invertedSet.bitmapRepresentation
+}
+
+for i in 0..<bitmap.length
+{
+    var byte:UInt8 = 0
+    bitmap.getBytes(&byte, range: NSRange(location: i, length: 1))
+    for n:UInt8 in 0..<8
+    {
+        let mask:UInt8 = 1 << n
+        if (mask & byte) > 0
+        {
+            let unicode = i * 8 + Int(n)
+            var message = String(format:"%04X  ", unicode)
+            if renderUnicodeAsText
+            {
+                message.append(UnicodeScalar(unicode))
+            }
+            
+            print(message)
+        }
+    }
+}
+
+if verbose
+{
+    for var i = 0; i < bitmap.length; i += column
+    {
+        var line = String(format:"%04X:  ", i)
+        for n in i..<min(i+column, bitmap.length)
+        {
+            let position = NSRange(location: n, length: 1)
+            
+            var byte = 0
+            bitmap.getBytes(&byte, range: position)
+            line += String(format: "%02X ", byte)
+            if (n + 1) % 4 == 0
+            {
+                line += " "
+            }
+        }
+        
+        print(line)
+    }
+    
+    print("Total: \(bitmap.length)")
+}
