1a4d0b2b7ca163874da9175b3ba6c3f6c7bf1cfe
larryhou <larryhou@foxmail.com>
2015-08-13 10:55:55 +0800

    MotionTracker.app

---
+++ MotionTracker/MotionTracker/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MotionTracker
+//
+//  Created by larryhou on 13/8/2015.
+//  Copyright © 2015 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
