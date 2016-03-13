3f1b857b5439755383977f6f7a5a011351790d0b
larryhou <larryhou@foxmail.com>
2016-03-13 14:37:54 +0800

    demo for bad performance with custom table cell of UITableViewController

---
+++ TableViewBadPerformanceSample/TableViewBadPerformanceSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewBadPerformanceSample
+//
+//  Created by larryhou on 13/3/2016.
+//  Copyright © 2016 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
