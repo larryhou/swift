78c6d46a8dec8cb6229c116751ffc7ce3a8e8dfe
larryhou.retina.imac <larryhou.retina.imac@foxmail.com>
2016-08-04 14:36:51 +0800

    crash report test

---
+++ CrashReport/CrashReport/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CrashReport
+//
+//  Created by larryhou on 8/4/16.
+//  Copyright © 2016 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        abort()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
