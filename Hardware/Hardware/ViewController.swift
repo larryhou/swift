172802e564a8ac92fa65000af7f21aba0d75a634
larryhou.retina.imac <larryhou.retina.imac@foxmail.com>
2017-07-11 20:48:38 +0800

    update

--- Hardware/Hardware/ViewController.swift
+++ Hardware/Hardware/ViewController.swift
@@ -51,6 +51,7 @@ class ViewController: UITableViewController
         data = [:]
         Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(reload), userInfo: nil, repeats: true)
         tableView.register(HeaderView.self, forHeaderFooterViewReuseIdentifier: HeaderView.identifier)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font.rawValue:UIFont(name: "Courier New", size: 30)!]
     }
     
     @objc func reload()
