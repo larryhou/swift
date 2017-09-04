a73c19195a9169337bd7d4417bd29120a7c716c8
larryhou <larryhou@larryhou-rmbp.local>
2017-09-04 19:14:23 +0800

    Vision.framework demo

---
+++ VisionPower/VisionPower/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionPower
+//
+//  Created by larryhou on 04/09/2017.
+//  Copyright © 2017 larryhou. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
